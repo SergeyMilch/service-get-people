75f20b86ed8a099a03d0c9eb0699c6eb2f10abe6
SergeyMilch <pushkin85.mil@gmail.com>
2024-01-25 15:02:42 +0300

    implementing recommendations 1,2,3

--- handlers/getPeople.go
+++ handlers/getPeople.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/SergeyMilch/service-get-people/models"
 	"github.com/SergeyMilch/service-get-people/utils/logger"
@@ -15,16 +17,62 @@ func GetPeople(db *sqlx.DB) gin.HandlerFunc {
 
         logger.Debug("Начало обработки запроса на получение всех пользователей", "func GetPeople")
 
+        // Получение и обработка параметров страницы и лимита из запроса
+        page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+        if err != nil || page < 1 {
+            page = 1
+        }
+        limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
+        if err != nil || limit <= 0 {
+            limit = 10
+        }
+
+        // Получение параметров фильтрации
+        nameFilter := c.Query("name")
+        surnameFilter := c.Query("surname")
+
+        // Формирование запроса с учетом фильтрации
+        baseQuery := "FROM people"
+        var args []interface{}
+        var conditions []string
+
+        if nameFilter != "" {
+            conditions = append(conditions, "name = ?")
+            args = append(args, nameFilter)
+        }
+        if surnameFilter != "" {
+            conditions = append(conditions, "surname = ?")
+            args = append(args, surnameFilter)
+        }
+
+        if len(conditions) > 0 {
+            baseQuery += " WHERE " + strings.Join(conditions, " AND ")
+        }
+
         var people []models.Person
-        err := db.Select(&people, "SELECT * FROM people")
+        query := "SELECT * " + baseQuery + " LIMIT ? OFFSET ?"
+        args = append(args, limit, (page-1)*limit)
+
+        err = db.Select(&people, query, args...)
         if err != nil {
             logger.Error("Ошибка при получении пользователей из базы", err.Error())
             c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
             return
         }
 
+        // Получение общего количества записей с учетом фильтров для вычисления общего числа страниц
+        var total int
+        countQuery := "SELECT COUNT(*) " + baseQuery
+        db.Get(&total, countQuery, args[:len(args)-2]...) // Исключаем параметры пагинации
+
         logger.Debug("Завершение обработки запроса на получение всех пользователей", "func GetPeople")
 
-        c.JSON(http.StatusOK, people)
+        // Отправка данных с информацией о пагинации
+        c.JSON(http.StatusOK, gin.H{
+            "data":       people,
+            "total":      total,
+            "page":       page,
+            "last_page":  (total + limit - 1) / limit,
+        })
     }
 }
--- models/models.go
+++ models/models.go
@@ -2,8 +2,8 @@ package models
 
 type Person struct {
     ID          uint   `db:"id" json:"id"`
-    UserName    string `db:"name" json:"name"`
-    Surname     string `db:"surname" json:"surname"`
+    UserName    string `db:"name" json:"name" binding:"required"`
+    Surname     string `db:"surname" json:"surname" binding:"required"`
     Patronymic  string `db:"patronymic" json:"patronymic"`
     Age         int    `db:"age" json:"age"`
     Gender      string `db:"gender" json:"gender"`
